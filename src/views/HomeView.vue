5f139d5f8c024ec44236dc03bc210162694653d1
ss0n1c <lls.s0n1cll@gamil.com>
2023-01-16 22:08:31 +0200

    add new project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+   <div class="conteiner">
+    <MainHeader/>
+   </div>
+  </div>
+</template>
+
+<script>
+import MainHeader from "@/components/MainHeader.vue";
+export default {
+  components: {
+    MainHeader,
+  }
+}
+</script>
+<style>
+
+</style>
